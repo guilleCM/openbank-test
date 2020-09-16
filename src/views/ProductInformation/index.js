cb12913b9ed4f8891fb5725a02b9ff16bf1d2901
guilleCM <guillermo.cirer@etoolinnovation.com>
2020-09-16 11:00:40 +0200

    initial commit

---
+++ src/views/ProductInformation/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Row from 'react-bootstrap/Container';
+
+// import step from './step1.png';
+
+function Step1 (props) {
+    return (
+        <Row>
+            <h1>Bienvenid@ a la Cuenta Corriente OpenClose</h1>
+        </Row>
+    );
+}
+
+export default Step1;
