ec2a37a43b433dcac6659e81bba58b31abe32c73
guilleCM <guillermo.cirer@etoolinnovation.com>
2020-09-18 21:16:07 +0200

    Updates

---
+++ precache-manifest.2c0b2e5be463a8e19bf8a1e29d8b1027.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c703d35640c9e227ce95e757632ce6d1",
+    "url": "/openbank-test/index.html"
+  },
+  {
+    "revision": "078ba8367c11166d5359",
+    "url": "/openbank-test/static/css/main.f3e8eca3.chunk.css"
+  },
+  {
+    "revision": "f28393512db09982190f",
+    "url": "/openbank-test/static/js/2.a2e41b62.chunk.js"
+  },
+  {
+    "revision": "078ba8367c11166d5359",
+    "url": "/openbank-test/static/js/main.fb04281b.chunk.js"
+  },
+  {
+    "revision": "ff0ecdf364b53dbd9c0f",
+    "url": "/openbank-test/static/js/runtime~main.25fb9028.js"
+  }
+]);
